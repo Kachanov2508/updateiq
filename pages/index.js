7fe0e7c599d2737aed48c7a7cd161a3c25a86d20
Kachanov2508 <kachanov2508@yandex.ru>
2022-01-09 19:54:26 +0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Navbar from "../components/NavBar/NavBar";
+import Navbar from "../components/Navbar/Navbar";
 import Layout from "../components/UI/Layout/Layout";
 
 export default function Home() {
