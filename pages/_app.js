76b561505c3f2f28f85c25889d59c4e503ab3a87
Kachanov2508 <kachanov2508@yandex.ru>
2022-01-09 18:44:35 +0300

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar/Navbar";
+// import Navbar from "../components/Navbar/Navbar";
 // import { NavbarProvider } from "../context/NavbarProvider";
 import { CourseProvider } from "../context/CourseProvider";
 
