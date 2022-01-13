8da15d2c6a17f5ff74e47c1e319d7a0425f640b3
kachanov2508 <kachanov2508@yandex.ru>
2022-01-13 14:02:26 +0300

    vercel

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     // domain: "http://localhost:3000",
-    domain: "https://updateiq.herokuapp.com",
+    // domain: "https://updateiq.herokuapp.com",
+    domain: "https://updateiq.vercel.app",
   }
 }
