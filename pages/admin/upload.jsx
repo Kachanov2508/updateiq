d5db0402177d7fe2ae5d6e07a7a74c57c0b801e0
Kachanov2508 <kachanov2508@yandex.ru>
2022-02-11 13:32:12 +0300

    ok

--- pages/admin/upload.jsx
+++ pages/admin/upload.jsx
@@ -1,43 +1,24 @@
-import { useState } from "react";
+// import { useState } from "react";
 import CreateCourse from "../../components/CreateCourse/CreateCourse";
 
-const Upload = () => {
-    const [login, setLogin] = useState("");
-    const [password, setPassword] = useState("");
-    const [admin, setAdmin] = useState(false);
+const upload = () => {
+    // const [login, setLogin] = useState("");
+    // const [password, setPassword] = useState("");
+    // const [admin, setAdmin] = useState(false);
 
-    function submitHandler(e) {
-        e.preventDefault();
+    // function submitHandler(e) {
+    //     e.preventDefault();
 
-        if (login === "Kachanov2508" && password === "Pasword2508")
-            setAdmin(true);
-    }
+    //     if (login === "Kachanov2508" && password === "Pasword2508")
+    //         setAdmin(true);
+    // }
 
     return (
         <div>
             <h1>Загрузка курсов</h1>
-            {admin ? (
-                <CreateCourse />
-            ) : (
-                <form onSubmit={submitHandler}>
-                    <input
-                        type="text"
-                        value={login}
-                        autoСomplete="off"
-                        onChange={(e) => setLogin(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        value={password}
-                        placeholder="password"
-                        autoСomplete="off"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button type="submit">Войти</button>
-                </form>
-            )}
+            <CreateCourse />
         </div>
     );
 };
 
-export default Upload;
+export default upload;
