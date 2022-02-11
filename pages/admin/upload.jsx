59c713d85b72014a1ff34ca8fbb42ed501480979
Kachanov2508 <kachanov2508@yandex.ru>
2022-02-11 12:46:59 +0300

    ok

--- pages/admin/upload.jsx
+++ pages/admin/upload.jsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import CreateCourse from "../../components/CreateCourse/CreateCourse";
 
 const upload = () => {
-	return (
-		<div>
-			<h1>Загрузка курсов</h1>
-			<CreateCourse />
-		</div>
-	);
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [admin, setAdmin] = useState(false);
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        if (login === "Kachanov2508" && password === "Pasword2508")
+            setAdmin(true);
+    }
+
+    return (
+        <div>
+            <h1>Загрузка курсов</h1>
+            {admin ? (
+                <CreateCourse />
+            ) : (
+                <form onSubmit={submitHandler}>
+                    <input
+                        type="text"
+                        value={login}
+                        autoСomplete="off"
+                        onChange={(e) => setLogin(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        value={password}
+                        placeholder="password"
+                        autoСomplete="off"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit">Войти</button>
+                </form>
+            )}
+        </div>
+    );
 };
 
 export default upload;
