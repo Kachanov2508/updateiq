014b51b4019bd36f86ecedd821fa15f033740e13
Kachanov2508 <kachanov2508@yandex.ru>
2022-02-11 13:23:45 +0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    domain: "http://localhost:3000",
-    // domain: "https://updateiq.herokuapp.com",
+    // domain: "http://localhost:3000",
+    domain: "https://updateiq.herokuapp.com",
     // domain: "https://updateiq.vercel.app",
   }
 }
