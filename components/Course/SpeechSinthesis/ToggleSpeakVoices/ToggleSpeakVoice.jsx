3427fc838c3bc7dea692c608d600eef4c8bf9433
Kachanov2508 <kachanov2508@yandex.ru>
2022-02-11 13:08:52 +0300

    ok

--- components/Course/SpeechSinthesis/ToggleSpeakVoices/ToggleSpeakVoice.jsx
+++ components/Course/SpeechSinthesis/ToggleSpeakVoices/ToggleSpeakVoice.jsx
@@ -2,14 +2,14 @@ import { faVolumeUp, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
 import Icon from "../../../UI/Icon/Icon";
 
 const ToggleSpeakVoice = (props) => {
-	
-	function toggleSpeakVoice() {
+
+	function clickHandler() {
 		window.speechSynthesis.cancel();
 		props.setSpeakVoice(!props.speakVoice);
 	}
 
 	return (
-		<div onClick={toggleSpeakVoice}>
+		<div onClick={clickHandler}>
 			{props.speakVoice ? (
 				<Icon icon={faVolumeMute} text="Русская озвучка" />
 			) : (
